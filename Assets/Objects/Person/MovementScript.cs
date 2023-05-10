8a9143e047387baef6e506f89f8bb97bbce99919
Seby Kiunke <seby.kiunke@googlemail.com>
2023-05-10 13:04:28 +0300

    Added Movement of 5 Seconds and Pause of 5 Seconds

--- Assets/Objects/Person/MovementScript.cs
+++ Assets/Objects/Person/MovementScript.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Objects.Person
@@ -7,18 +6,37 @@ namespace Objects.Person
     {
         private Animator _animator;
         private static readonly int Speed = Animator.StringToHash("Speed");
+        private float _timer;
+        private const float WalkDuration = 5f;
+        private const float IdleDuration = 10f;
+
 
         private void Start()
         {
             _animator = GetComponent<Animator>();
-
         }
 
         private void Update()
         {
-            var velocity = Vector3.forward * 2.0f;
-            transform.Translate(velocity * Time.deltaTime);
-            _animator.SetFloat(Speed, velocity.magnitude);
+            if (!(_timer < WalkDuration))
+            {
+                if (_timer < IdleDuration)
+                {
+                    _animator.SetFloat(Speed, 0.001f);
+                    _timer += Time.deltaTime;
+                }
+                else
+                {
+                    _timer = 0;
+                }
+            }
+            else
+            {
+                var velocity = Vector3.forward * 2.0f;
+                transform.Translate(velocity * Time.deltaTime);
+                _animator.SetFloat(Speed, velocity.magnitude);
+                _timer += Time.deltaTime;
+            }
         }
     }
-}   
+}
